42a0d471173335966ab9d36143b9d2ac3b75c3a8
Jamie <flare2399@gmail.com>
2019-02-16 20:51:56 +0000

    [staff] add pm cmd

--- staff/staff.py
+++ staff/staff.py
@@ -62,3 +62,57 @@ class Staff(BaseCog):
                 embed.add_field(name="Lead Admin", value=member.display_name, inline=True)
         embed.add_field(name="\N{ZERO WIDTH SPACE}", value="\N{ZERO WIDTH SPACE}", inline=True)
         await ctx.send(embed=embed)
+
+    @commands.command()
+    @commands.has_any_role("Moderator", "IG Moderator", "Server Management", "Server Owner", "Senior Administrator",
+                           "Administrator")
+    async def pm(self, ctx: commands.Context, user_id: int, *, message: str):
+        """Sends a DM to a user
+
+        This command needs a user id to work.
+        To get a user id enable 'developer mode' in Discord's
+        settings, 'appearance' tab. Then right click a user
+        and copy their id"""
+        destination = discord.utils.get(ctx.bot.get_all_members(), id=user_id)
+        if destination is None:
+            await ctx.send(
+                _(
+                    "Invalid ID or user not found. You can only "
+                    "send messages to people I share a server "
+                    "with."
+                )
+            )
+            return
+
+        fake_message = namedtuple("Message", "guild")
+        prefixes = await ctx.bot.command_prefix(ctx.bot, fake_message(guild=None))
+        prefix = prefixes[0]
+        description = _("Reply from WC-RP Staff Member {}").format(ctx.author.display_name)
+        content = _("You can reply to this message just by replying here.")
+        if await ctx.embed_requested():
+            e = discord.Embed(colour=discord.Colour.red(), description=message)
+
+            e.set_footer(text=content)
+            if ctx.bot.user.avatar_url:
+                e.set_author(name=description, icon_url=ctx.bot.user.avatar_url)
+            else:
+                e.set_author(name=description)
+
+            try:
+                await destination.send(embed=e)
+            except discord.HTTPException:
+                await ctx.send(
+                    _("Sorry, I couldn't deliver your message to {}").format(destination)
+                )
+            else:
+                await ctx.send(_("Message delivered to {}").format(destination))
+        else:
+            response = "{}\nMessage:\n\n{}".format(description, message)
+            try:
+                await destination.send("{}\n{}".format(box(response), content))
+            except discord.HTTPException:
+                await ctx.send(
+                    _("Sorry, I couldn't deliver your message to {}").format(destination)
+                )
+            else:
+                await ctx.send(_("Message delivered to {}").format(destination))
